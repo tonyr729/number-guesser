f61bbb9a5aa9677be8a0e4f444f88acd7204daf0
Tony Robertson <tonyr729@gmail.com>
2018-01-31 00:30:39 -0700

    CSS change and image removal
    Decided to remove the image and display the users option in range through border color of the extra input fields. Also cleaned up the code a lot and assigned more variables.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,26 @@
+//*=======================================
+//* TODO
+//*=======================================
+//* refactor
+//* variable math.random 
+//* incrimental range -10 +10 on every win
+//* aria functionality
 
 
 
+//Variable Range---------------------------------------------------------------------------------------------------! Not working
+// formInput.addEventListener('focus', getRandomArbitrary, false);
 
-
-//TODO
-// fix/delete main();
-// refactor
-// assign variables for legability
-// CSS- idomatic property arrangment 
-// variable math.random 
-// incrimental range -10 +10 on every win
-
-
-
-
-
-
-
-
+// function getRandomArbitrary() {
+//   console.log(formInputMin.length);
+//   console.log(formInputMax.length);
+//   if (formInputMin.value.length > 0 && formInputMax.value.length > 0) {
+//     console.log('success!')
+//     randomNumber=Math.floor(Math.random() * (minNumber - maxNumber) + minNumber);
+//   } else {
+//     randomNumber=randomNumber;
+//   }
+// }
 
 
 
@@ -27,167 +30,150 @@
 var randomNumber = Math.floor(Math.random() * 99)+1
 var minNumber = 1
 var maxNumber = 100
-var formInput = document.getElementById('form__input')
-var formInputMin = document.getElementById('form__input-min')
-var formInputMax = document.getElementById('form__input-max')
+var formInput = document.querySelector('#form__input')
+var formInputMin = document.querySelector('#form__input-min')
+var formInputMax = document.querySelector('#form__input-max')
+var inputLabelMin = document.querySelector('#input__label-min')
+var inputLabelMax = document.querySelector('#input__label-max')
+var buttonClear = document.querySelector('#button__clear')
+var buttonReset = document.querySelector('#button__reset')
+var buttonGuess = document.querySelector('#button__guess')
+var pGuess = document.querySelector('#p__guess')
+var bigText = document.querySelector('#big__text')
+var pWarning = document.querySelector('#p__warning')
+var pTooHighTooLow = document.querySelector('#p__high')
+var pCongrats = document.querySelector('#form__p-congrats')
 
+//Designates userNumber 
 formInput.addEventListener('input',changeNum,false);
-  function changeNum() {
-    userNumber=formInput.value;
+function changeNum() {
+  userNumber=formInput.value;
 }
 
 //Hide Advanced Features
 hideAdvanced();
 
 function hideAdvanced() {
-  document.getElementById('input__label-min').style.display='none';
-  document.getElementById('input__label-max').style.display='none';
-  document.getElementById('hidden__img').style.display='none';
+  inputLabelMin.style.display='none';
+  inputLabelMax.style.display='none';
 }
 
-// main();
+//Button Disable
+formInput.addEventListener('keyup', formReset, false);
+formInput.addEventListener('click', formReset, false);
 
-// function main(){
- //Assign Input to Variable to use in Boolean for restriction ----------------------------------------------------------! Works but not inside Boolean
-
-
-  //CHANGE RANGE
-  document.getElementById('form__input-min').addEventListener('blur',changeRangeMin,false);
-
-  function changeRangeMin() {
-    minNumber=this.value;
-    // main();
+function formReset() {
+  if(formInput.value.length > 0) { 
+    buttonClear.disabled = false;
+    buttonReset.disabled = false;
+    buttonGuess.disabled = false;
+  } else { 
+    buttonClear.disabled = true;
+    buttonGuess.disabled = true;
+    resetAll();
   }
-  document.getElementById('form__input-max').addEventListener('blur',changeRangeMax,false);
-
-  function changeRangeMax() {
-    maxNumber=this.value;
-    // main();
+}
+function resetAll() {
+  if(userNumber.length >0) {
+    buttonReset.disabled = false;
+  } else {
+    buttonReset.disabled = true;
   }
-  //Variable Range---------------------------------------------------------------------------------------------------! Not working
-  // formInput.addEventListener('focus', getRandomArbitrary, false);
+}
 
-  // function getRandomArbitrary() {
-  //   console.log(formInputMin.length);
-  //   console.log(formInputMax.length);
-  //   if (formInputMin.value.length > 0 && formInputMax.value.length > 0){
-  //     console.log('success!')
-  //     randomNumber=Math.floor(Math.random() * (minNumber - maxNumber) + minNumber);
-  //   } else {
-  //     randomNumber=randomNumber;
-  //   }
-  // }
-  
-
-  //Prevent the E key
-  formInput.addEventListener('keypress', function (evt) {
-      if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57) {
-        evt.preventDefault();
-      }
-  });
-
-
-  //Button Disable
-
-  formInput.addEventListener('keyup', formReset, false);
-
-  function formReset(){
-    if(formInput.value.length > 0) { 
-      document.getElementById('button__clear').disabled = false;
-      document.getElementById('button__reset').disabled = false;
-      document.getElementById('button__guess').disabled = false;
-    } else { 
-      document.getElementById('button__clear').disabled = true;
-      document.getElementById('button__guess').disabled = true;
-      resetAll();
-    }
+//Prevent the E key
+formInput.addEventListener('keypress', function (evt) {
+  if (evt.which != 8 && evt.which != 0 && evt.which < 48 || evt.which > 57) {
+    evt.preventDefault();
   }
-  function resetAll(){
-    if(userNumber.length >0) {
-      document.getElementById('button__reset').disabled = false;
-    } else {
-      document.getElementById('button__reset').disabled = true;
-    }
+});
+
+//Guess Button - Top text replace - Number insert
+buttonGuess.addEventListener('click', display_num);
+
+function display_num() {
+  event.preventDefault();
+  checkNum();
+  formReset();
+}
+
+//Cear Button
+buttonClear.addEventListener('click', clear);
+
+function clear() {
+  event.preventDefault();
+  formReset();
+  formInput.value= '';
+  formReset();
+}
+
+//Reset Button
+buttonReset.addEventListener('click', reset);
+
+function reset() {
+  pGuess.innerHTML='Guess a number between1 and 100';
+  bigText.innerHTML='';
+  pTooHighTooLow.innerHTML='';
+  buttonReset.setAttribute('disabled', true);
+  hideAdvanced();
+  formReset();
+}
+
+//CHANGE RANGE
+formInputMin.addEventListener('blur',changeRangeMin,false);
+
+function changeRangeMin() {
+  minNumber=this.value;
+}
+
+formInputMax.addEventListener('blur',changeRangeMax,false);
+
+function changeRangeMax() {
+  maxNumber=this.value;
+}
+
+// User# === Random# 
+function displayResult() {
+  pGuess.innerHTML='Your last guess was';
+  bigText.innerHTML=parseInt(formInput.value);
+  formInput.value = '';
+}
+function checkNum() {
+  console.log(randomNumber);
+  var userNumber = parseInt(formInput.value);
+  var outOfRangeMsg =pWarning;
+  if (userNumber > maxNumber || userNumber < minNumber) {
+    outOfRangeMsg.textContent = '* That number is outside the designated range';
+    formInput.value = '';
+  } else if (randomNumber === userNumber) {
+    randomNumber = Math.floor(Math.random() * 99)+1;
+    pTooHighTooLow.innerHTML='Boom!';
+    inputLabelMin.style.display= '';
+    inputLabelMax.style.display= '';
+    pCongrats.innerHTML= 'Congrats! Pick your own range.';
+    displayResult();
+  } else{
+    highLow();
   }
-  
+}
 
-  //Guess Button - Top text replace - Number insert
-  document.getElementById('button__guess').addEventListener('click', display_num);
-
-  function display_num() {
-    event.preventDefault();
-    checkNum();
-    formReset();
-  }
-
-  //Cear Button-----------------------------------------------------------------------------------------------------! Clear Phase 3
-  document.getElementById('button__clear').addEventListener('click', clear);
-
-  function clear() {
-    event.preventDefault();
-    formReset();
-    formInput.value= '';
-    formReset();
-
-  }
-
-  //Reset Button----------------------------------------------------------------------------------------------------! Rest Phase 3
-  document.getElementById('button__reset').addEventListener('click', reset);
-
-  function reset() {
-    document.getElementById('p__guess').innerHTML='Guess a number between1 and 100';
-    document.getElementById('big__text').innerHTML='';
-    document.getElementById('p__high').innerHTML='';
-    document.getElementById('button__reset').setAttribute('disabled', true);
-    hideAdvanced();
-    formReset();
-  }
-
- // User# === Random# 
-  function displayResult() {
-    document.getElementById('p__guess').innerHTML='Your last guess was';
-    document.getElementById('big__text').innerHTML=parseInt(formInput.value);
+//User guess too high/too low
+function highLow() {
+  var userNumber = parseInt(formInput.value);
+  if (randomNumber > userNumber) {
+    pTooHighTooLow.innerHTML='That is too low';
+    displayResult();
+    formInput.value = '';
+  } else if (randomNumber < userNumber) {
+    pTooHighTooLow.innerHTML='That is too high';
+    displayResult();
     formInput.value = '';
   }
-  function checkNum() {
-      console.log('checkNum');
-      console.log(randomNumber);
-      var userNumber = parseInt(formInput.value);
-      var outOfRangeMsg =document.getElementById('p__warning');
-      if (userNumber > maxNumber || userNumber < minNumber) {
-        outOfRangeMsg.textContent = '* That number is outside the designated range';
-        formInput.value = '';
-      } else if (randomNumber === userNumber) {
-          randomNumber = Math.floor(Math.random() * 99)+1;
-          document.getElementById('p__high').innerHTML='Boom!';
-          document.getElementById('input__label-min').style.display= '';
-          document.getElementById('input__label-max').style.display= '';
-          document.getElementById('hidden__img').style.display= '';
-          displayResult();
-      } else{
-          highLow();
-        }
-  }
+}
 
-  //Error Reset
-  formInput.addEventListener('keyup', errorReset, false);
+//Error Reset
+formInput.addEventListener('keyup', errorReset, false);
 
-  function errorReset() {
-    document.getElementById('p__warning').innerHTML='';
-  }
-
-  //Shows if the number was high or low
-  function highLow() {
-      console.log('highLow')
-      var userNumber = parseInt(formInput.value);
-      if (randomNumber > userNumber) {
-        document.getElementById('p__high').innerHTML='That is too low';
-        displayResult();
-        formInput.value = '';
-      } else if (randomNumber < userNumber) {
-        document.getElementById('p__high').innerHTML='That is too high';
-        displayResult();
-        formInput.value = '';
-      }
-  }
-// }
+function errorReset() {
+  pWarning.innerHTML='';
+}
